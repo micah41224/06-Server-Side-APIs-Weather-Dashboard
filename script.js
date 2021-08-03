e07dfba308d2344195850f106d3e0e9bb53cfafd
Mike <micah41224@gmail.com>
2021-08-03 14:06:09 -0400

    added most of the variables needed in the script file

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 function loadAll() {
     
     const APIKey = "e441c40d7c8015427300822498b53fc2";
+
+
+    var cityDateDisplay = "";
     var cityName = "atlanta"
+    
+    var nameList = document.getElementById("city-name");
+    var currentPicList = document.getElementById("current-pic");
+    var currentTempList = document.getElementById("temp");
+    var currentHumidityList = document.getElementById("hum");
+    var currentWindList = document.getElementById("wind-speed");
+    var currentUVList = document.getElementById("UV-index");
+    var cityList = document.getElementById("enter-city");
+    var searchList = document.getElementById("search-button");
+    var clearList = document.getElementById("clear-history");
+    var historyList = document.getElementById("history");
+    var fivedayList = document.getElementById("fiveday-header");
+    var todayweatherList = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+
+function getWeather(cityName) {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         axios.get(queryURL)
-        console.log(queryURL);
-}
+            
+       // console.log(queryURL);
 
+}
+}
 loadAll();
