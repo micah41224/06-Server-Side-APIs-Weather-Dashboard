328bb87ae5bcf88d4a36367944088ceda5ce78a2
Mike <micah41224@gmail.com>
2021-08-05 11:12:28 -0400

    Continuing to play with initial function and variables to call/report weather data

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function loadAll() {
     
     const APIKey = "e441c40d7c8015427300822498b53fc2";
@@ -25,8 +26,9 @@ function getWeather(cityName) {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         axios.get(queryURL)
             
-       // console.log(queryURL);
+getWeather();
+console.log(getWeather);
 
-}
+    }
 }
 loadAll();
