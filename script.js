b20befe995e75936565db69b5a46b6ffb9b17b2f
Mike <micah41224@gmail.com>
2021-08-05 22:59:42 -0400

    cleaned out old code approach, preserved apikey note

--- script.js
+++ script.js
@@ -45,53 +45,7 @@ button.addEventListener('click',function(){
 
 
 
-/*
-function loadAll() {
-    
-    const APIKey = "e441c40d7c8015427300822498b53fc2";
 
-    console.log(APIKey);
+//    const APIKey = "e441c40d7c8015427300822498b53fc2";
 
-    var cityDateDisplay = "";
-    const cityName = "atlanta"
-    
-    var nameList = document.getElementById("city-name");
-    var currentPicList = document.getElementById("current-pic");
-    var currentTempList = document.getElementById("temp");
-    var currentHumidityList = document.getElementById("hum");
-    var currentWindList = document.getElementById("wind-speed");
-    var currentUVList = document.getElementById("UV-index");
-    var cityList = document.getElementById("enter-city");
-    var searchList = document.getElementById("search-button");
-    var clearList = document.getElementById("clear-history");
-    var historyList = document.getElementById("history");
-    var fivedayList = document.getElementById("fiveday-header");
-    var todayweatherList = document.getElementById("today-weather");
-    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-
-
-function getWeather(cityName) {
-    console.log(cityName);
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-        axios.get(queryURL)
-    
-       })  
-     //  getWeather();
-    } 
-    getWeather();
-}
-//getWeather();
-//console.log(getWeather);
-
-  //  }
-//}
-
-
-
-
-document.querySelector('#search-btn').addEventListener('click', loadAll);
-
-
-//loadAll();
-
-*/
+   
