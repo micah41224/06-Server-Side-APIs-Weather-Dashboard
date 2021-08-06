784d9e13a7d4a6c74dc2fe0108d9824f87aa7012
Mike <micah41224@gmail.com>
2021-08-05 22:47:07 -0400

    used different syntax to load humidity into html but is now working

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 var button = document.querySelector('.button');
 var inputValue = document.querySelector('.inputValue');
 var nameSlot = document.querySelector('.cityName');
-var wind = document.querySelector('.wind');
 var temp = document.querySelector('.temp');
+var humidity = document.querySelector('.humidity');
+var wind = document.querySelector('.wind');
+
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=e441c40d7c8015427300822498b53fc2')
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         var nameValue = data['name'];
+        var currentDate = new Date();
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth() +1;
+        var year = currentDate.getFullYear();
         var tempValue = data['main']['temp'];
+        var humidityValue = data.main.humidity;
+        console.log(humidityValue);
         var windValue = data['wind']['speed'];
         var tempConverted =  ((tempValue - 273.15) * 1.8 + 32);
         var tempRounded = Math.round(tempConverted);
 
-        nameSlot.innerHTML ="City: "+nameValue;
+        nameSlot.innerHTML ="City: "+nameValue+" (" + month + "/" + day + "/" + year + ") ";
         temp.innerHTML = "Temp: "+tempRounded+"°";
+        humidity.innerHTML = "Humidity: "+humidityValue+"%";
         wind.innerHTML = "Wind Speed: "+windValue+" MPH"
     })
 })
 
 
 
+/*
+const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
 
-
-
-
-
-
+*/
 
 
 
